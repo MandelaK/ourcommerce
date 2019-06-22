d83443224611129ba14ab6ce6eaec9ee30b791f3
Keith <44587521+MandelaK@users.noreply.github.com>
2019-06-22 10:15:33 +0300

    Initial forms (#1)
    * ft(contact form): Create contact form
    
    - Create contact form
    - Create templates for contact and about page
    - Create views for contact and about page
    
    * ft(forms) Set up login and registration forms and endpoints
    
    - Set up login form and views
    - Set up registration form and views

---
+++ src/core/forms.py
@@ -0,0 +1,74 @@
+from django import forms
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class ContactForm(forms.Form):
+    """Define the contact form information"""
+    full_name = forms.CharField(label='Full Names', widget=forms.TextInput(
+        attrs={"class": "form-control", "placeholder": "Your full names"}))
+    email = forms.EmailField(label='Email', widget=forms.EmailInput(
+        attrs={"class": "form-control", "placeholder": "Your email"}))
+    content = forms.CharField(
+        label='Content', widget=forms.Textarea(
+            attrs={"class": "form-control", "placeholder": "Your message"}))
+
+
+class LoginForm(forms.Form):
+    """Define the login form"""
+    username = forms.CharField(
+        label='Username', widget=forms.TextInput(
+            attrs={"class": "form-control", "placeholder": "Enter your username..."}))
+    password = forms.CharField(
+        label='Password', widget=forms.PasswordInput(
+            attrs={"class": "form-control", "placeholder": "Enter your password..."})
+    )
+
+
+class RegisterForm(forms.Form):
+    """Define the registration form"""
+
+    email = forms.EmailField(
+        label='Email', widget=forms.EmailInput(
+            attrs={"class": "form-control", "placeholder": "Enter your email..."})
+    )
+    username = forms.CharField(
+        label='Username', widget=forms.TextInput(
+            attrs={"class": "form-control", "placeholder": "Enter your username..."}))
+    password = forms.CharField(
+        label='Password', widget=forms.PasswordInput(
+            attrs={"class": "form-control", "placeholder": "Enter your password..."})
+    )
+    password2 = forms.CharField(
+        label='Confirm Password', widget=forms.PasswordInput(
+            attrs={"class": "form-control", "placeholder": "Confirm your password..."})
+    )
+
+    def clean_username(self):
+        """Ensure usernames are unique"""
+        username = self.cleaned_data.get('username')
+        query = User.objects.filter(username=username)
+        if query.exists():
+            raise forms.ValidationError(
+                "A user with this username exists already"
+            )
+        return username
+
+    def clean_email(self):
+        """Ensure usernames are unique"""
+        email = self.cleaned_data.get('email')
+        query = User.objects.filter(email=email)
+        if query.exists():
+            raise forms.ValidationError(
+                "A user with this email exists already"
+            )
+        return email
+
+    def clean(self):
+        data = self.cleaned_data
+        password = self.cleaned_data.get('password')
+        password2 = self.cleaned_data.get('password2')
+        if password != password2:
+            raise forms.ValidationError("Passwords must match!")
+        return data
