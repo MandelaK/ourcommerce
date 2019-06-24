bc8b5e605ecd8a377003ddd22bc1c8bf9d2b51b7
MandelaK <kazi.keith@gmail.com>
2019-06-24 19:32:11 +0300

    ft(templates) Set up routes and refactor templates
    - Create navbar with valid links
    - Refactor templates
    - Refactor urls

--- src/products/urls.py
+++ src/products/urls.py
@@ -5,7 +5,7 @@ from products.views import (
 
 urlpatterns = [
     path('', ProductListView.as_view(), name='list_products'),
-    path('<slug>/', ProductDetailView.as_view(), name='single_product'),
+    path('<slug>/', ProductDetailView.as_view(), name='product_detail'),
     path('f/featured/', ProductFeaturedListView.as_view(),
          name='featured_products'),
     path('featured/<pk>/', ProductFeaturedDetailView.as_view(),
