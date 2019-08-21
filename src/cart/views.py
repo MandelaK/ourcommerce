ac91f2ad1d814aa482ab7c08212f0baf56436588
MandelaK <kazi.keith@gmail.com>
2019-08-21 15:44:33 +0300

    ft(Cart) Implement Cart App
    - Create Cart Model and Managers
    - Implement signals to udpate the Cart totals whenever products are added
    - Create logic for updating the cart
    - Users don't loose their cart when they log in

---
+++ src/cart/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render, redirect
+
+from products.models import Product
+from .models import Cart
+
+
+def cart_home(request):
+    """
+    Endpoint for retreiving and/or creating the
+    user's cart
+    """
+    cart_obj, new_obj = Cart.objects.new_or_get(request)
+    return render(request, "cart/home.html", {"cart": cart_obj})
+
+
+def cart_update(request):
+    product_id = request.POST.get('product_id')
+    if product_id is not None:
+        product_obj = Product.objects.get(id=product_id)
+        cart_obj, new_obj = Cart.objects.new_or_get(request)
+        if product_obj in cart_obj.products.all():
+            cart_obj.products.remove(product_obj)
+        else:
+            cart_obj.products.add(product_obj)
+        request.session['cart_items'] = cart_obj.products.count()
+    return redirect("cart:cart")
--- src/core/urls.py
+++ src/core/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import home_page, contact_page, about_page, login_page, register_page
+from .views import home_page, contact_page, about_page, login_page, register_page, logout
 
 urlpatterns = [
     path('', home_page, name='home'),
@@ -7,4 +7,5 @@ urlpatterns = [
     path('contact/', contact_page, name='contact'),
     path('login/', login_page, name='login'),
     path('register/', register_page, name='register'),
+    path('logout/', logout, name='logout')
 ]
