f937e50b0b16af7a4efdb2c0f19e6cd41eab910a
MandelaK <kazi.keith@gmail.com>
2019-09-12 15:31:20 +0300

    ft(Products AJAX) Allow asynchronous updating of the products cart
    - Cart is now updated asynchronously whenever the cart is updated

--- src/cart/views.py
+++ src/cart/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.http import JsonResponse
 
 from .models import Cart
 from accounts.forms import LoginForm, GuestForm
@@ -24,16 +25,21 @@ def cart_update(request):
     View for updating the cart
     """
     product_id = request.POST.get('product_id')
-    if request.is_ajax:
-        print("Ajax request")
     if product_id is not None:
         product_obj = Product.objects.get(id=product_id)
         cart_obj, new_obj = Cart.objects.new_or_get(request)
         if product_obj in cart_obj.products.all():
             cart_obj.products.remove(product_obj)
+            product_added = False
         else:
             cart_obj.products.add(product_obj)
+            product_added = True
         request.session['cart_items'] = cart_obj.products.count()
+        if request.is_ajax:
+            json_data = {
+                "added": product_added,
+            }
+            return JsonResponse(json_data)
     return redirect("cart:cart")
 
 
